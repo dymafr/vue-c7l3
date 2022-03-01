099c526c004adca2bd4c9352785e85fe9a7f42ad
dymafr <erwan1309@hotmail.com>
2022-03-01 12:57:18 +0100

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <Product />
-  <Product />
-  <Product />
+  <Product :name="product" />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import Product from './Product.vue';
+
+const product = ref('Voiture');
 </script>
 
 <style scoped lang="scss"></style>
--- src/Product.vue
+++ src/Product.vue
@@ -1,16 +1,9 @@
 <template>
-  <h3>Voiture</h3>
-  <ul>
-    <li>Prix: 1500€</li>
-  </ul>
-  <button @click="count++">Ajouter une voiture au panier</button>
-  <p>Vous avez ajouté {{ count }} voiture</p>
+  <h3>{{ name }}</h3>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const count = ref(0);
+defineProps(['name']);
 </script>
 
 <style scoped lang="scss"></style>
